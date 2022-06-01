7e5f76607acbf15865a648263063fbd9a1c9e353
Cem Tanrikut <cemtanrikut@gmail.com>
2022-06-01 22:28:28 +0300

    Added customer items

--- api/customer/customer.go
+++ api/customer/customer.go
@@ -1,4 +1,24 @@
 package customer
 
+import "time"
+
 type CustomerData struct {
+	CustomerID           string              `json:"customer_id"`
+	Name                 string              `json:"name"`
+	Surname              string              `json:"surname"`
+	Mail                 string              `json:"mail"`
+	Phone                string              `json:"phone"`
+	Age                  time.Time           `json:"age"`
+	IdentificationNumber string              `json:"identification_number"` //TODO Should masked
+	Status               string              `json:"status"`
+	CreateDate           time.Time           `json:"create_date"`
+	Address              CustomerAddressData `json:"address"`
+}
+
+type CustomerAddressData struct {
+	Country     string `json:"country"`
+	City        string `json:"city"`
+	District    string `json:"district"`
+	PostalCode  string `json:"postal_code"`
+	LongAddress string `json:"long_address"`
 }
